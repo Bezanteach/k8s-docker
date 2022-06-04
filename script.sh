ed6d4011107abb3455a03017aa91fd7f0e022e75
nguelewie <tenangfie100@gmail.com>
2022-06-04 08:34:05 -0400

    script updated

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if 
-  [ {$UID} != 0 ]
+  [ ${UID} != 0 ]
 then 
-echo " You need root access"
+echo "You need root access"
 else
 echo "You good to go ... be gentle when navigating please"
 fi
